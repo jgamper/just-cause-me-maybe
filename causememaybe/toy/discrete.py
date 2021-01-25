262b56ea9d1a51d0a6ead3bd9be34137176a14cc
jgamper <jevgenij.gamper5@gmail.com>
2021-01-25 23:37:54 +0200

    corrected for mean estimation given the coef and G

--- causememaybe/toy/discrete.py
+++ causememaybe/toy/discrete.py
@@ -8,10 +8,12 @@ beta_0_func = lambda X: np.exp(0.7 - 0.3 * X[:, 0] + 0.8 * X[:, 1])
 beta_1_func = lambda X: np.exp(0.9 - 0.5 * X[:, 0] + 0.5 * X[:, 1])
 
 
-def sample_rho(size, g, alphas, betas, g_coef: float = 0.2):
+def sample_rho(size, g, alphas, betas, coef: float = 0.2):
     ones = np.ones(size)
-    rho = np.random.beta(alphas, betas, size=size)
-    rho = np.vstack([rho + g_coef * g, ones]).transpose()
+    rho_1 = np.random.beta(alphas, betas, size=size)
+    rho_2 = 0.65*g + 0.35*(1-g)
+    rho = (1-coef)*rho_1 + coef*rho_2
+    rho = np.vstack([rho, ones]).transpose()
     rho = np.min(rho, axis=1)
     return rho
 
@@ -33,6 +35,7 @@ class DiscreteToyProcess:
         noise: float = 0.4,
         random_state: int = 0,
         overlap: str = "random",
+        coef: float = 0.2
     ):
         """
 
@@ -40,12 +43,14 @@ class DiscreteToyProcess:
         :param noise:
         :param random_state:
         :param overlap: one of ["random", "strong", "moderate", "weak"]
+        :param coef:
         """
         assert overlap in ["random", "strong", "moderate", "weak"]
         self.n_samples = n_samples
         self.noise = (noise,)
         self.random_state = random_state
         self.overlap = overlap
+        self.coef = coef
         if self.overlap != "random":
             self._propensity_scale = {"strong": 0.5, "moderate": 2, "weak": 4}[
                 self.overlap
@@ -95,23 +100,23 @@ class DiscreteToyProcess:
             treatment = np.random.binomial(n=1, p=treatment_prob)
             return treatment, treatment_prob
 
-    def sample_outcomes(self, treatment: str, median=False):
+    def sample_outcomes(self, treatment: str):
         size = self.X.shape[0]
         if treatment == "test":
             betas = beta_1_func(self.X)
             alpha = 3
             alphas = np.ones(size) * alpha
             rho = sample_rho(
-                size, self.latent_binary_confounder, alphas, betas, g_coef=0.2
+                size, self.latent_binary_confounder, alphas, betas, coef=self.coef
             )
         elif treatment == "control":
             betas = beta_1_func(self.X)
             alpha = 1
             alphas = np.ones(size) * alpha
             rho = sample_rho(
-                size, self.latent_binary_confounder, alphas, betas, g_coef=0.2
+                size, self.latent_binary_confounder, alphas, betas, coef=self.coef
             )
 
         y = np.random.binomial(1, rho, size)
-        mu = 1 / (1 + betas / alphas)
+        mu = 1 / (1 + betas / alphas) * (1-self.coef) + self.coef*self.latent_binary_confounder
         return y, rho, mu
